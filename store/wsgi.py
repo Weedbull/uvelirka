1334d9f6154070e86f202cd918199fd79b4af228
Weedbull <93524480+Weedbull@users.noreply.github.com>
2023-04-27 16:04:45 +0200

    ye

--- store/wsgi.py
+++ store/wsgi.py
@@ -1,2 +1,9 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelryshop.settings')
 # vercel_app/settings.py
 WSGI_APPLICATION = 'store.wsgi.app'
+application = get_wsgi_application()
+
+app = application
