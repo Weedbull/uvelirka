aaf82b1494c66544e03a0c0f5d9a50820c78ab5f
Weedbull <93524480+Weedbull@users.noreply.github.com>
2023-04-27 15:00:01 +0200

    y

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -64,8 +64,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'jewelryshop.wsgi.application'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 
 
 # Database
